0dfd11406419918bbe285dc2d609a3a929b6f3e5
Omar Khan <omarkhan@Omars-MacBook-Pro.local>
2018-11-08 14:55:22 +0000

    added energy usage

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,18 +2,32 @@
 
 function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
-  this.temperature = 20;
+  this.DEFAULT_TEMPERATURE = 20;
+  this.temperature = this.DEFAULT_TEMPERATURE;
   this.powerSavingMode = true;
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
+  this.MEDIUM_USAGE_LIMIT = 18;
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
 };
 
-Thermostat.prototype.isMinimumTemperature = function() {
+Thermostat.prototype.isMinimumTemperature = function () {
   return this.temperature === this.MINIMUM_TEMPERATURE;
+};
+
+Thermostat.prototype.isPowerSavingModeOn = function () {
+  return this.powerSavingMode === true;
+};
+
+Thermostat.prototype.switchPowerSavingModeOff= function () {
+  this.powerSavingMode = false;
+};
+
+Thermostat.prototype.switchPowerSavingModeOn = function () {
+  this.powerSavingMode = true;
 };
 
 Thermostat.prototype.isMaximumTemperature = function() {
@@ -24,6 +38,9 @@ Thermostat.prototype.isMaximumTemperature = function() {
   return this.temperature === this.MAX_LIMIT_PSM_ON;
 };
 
+Thermostat.prototype.resetTemperature = function() {
+  this.temperature = this.DEFAULT_TEMPERATURE;
+};
 Thermostat.prototype.up = function() {
   if (this.isMaximumTemperature()) {
     return;
@@ -38,14 +55,12 @@ Thermostat.prototype.down = function () {
   this.temperature -= 1;
 };
 
-Thermostat.prototype.isPowerSavingModeOn = function() {
-  return this.powerSavingMode === true;
-};
-
-Thermostat.prototype.switchPowerSavingModeOff= function() {
-  this.powerSavingMode = false;
-};
-
-Thermostat.prototype.switchPowerSavingModeOn = function() {
-  this.powerSavingMode = true;
+Thermostat.prototype.energyUsage = function() {
+  if (this.temperature < this.MEDIUM_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temperature >= this.MEDIUM_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
 };
